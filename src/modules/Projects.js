fe3511c25cc48633b28cf06881215aae1093a957
James Blankenship <jbla484@wgu.edu>
2023-10-12 17:50:22 -0700

    Added a project to the projects page

--- src/modules/Projects.js
+++ src/modules/Projects.js
@@ -6,34 +6,38 @@ export default function Projects() {
                     <h2>PROJECTS</h2>
                 </div>
 
-                <div style={{ textAlign: 'center', marginBottom: '20px' }}>
-                    <h3 style={{ marginBottom: '15px' }}>CryptoStats</h3>
+                <div
+                    className='projectContainer'
+                    style={{ textAlign: 'center', marginBottom: '20px' }}
+                >
+                    <h3 style={{ padding: '15px 0' }}>CryptoStats</h3>
                     <img
+                        className='projectImage'
                         src={require('../images/cryptostats.png')}
                         alt='me'
-                        width={'100%'}
-                        height={'00%'}
-                        style={{
-                            borderRadius: '10px',
-                        }}
+                        width={'80%'}
+                        height={'80%'}
+                        style={{}}
                     ></img>
-                    <p style={{ margin: '10px 0' }}>
-                        Displays all popular crypto currency data in an easy to
-                        use and readable table. This data is pulled from an
-                        external API.{' '}
+                    <p style={{ margin: '10px auto', width: '80%' }}>
+                        Using React JS, this application displays all popular
+                        crypto currency data in an easy to use, readable table.
+                        This data is pulled from the external crypto coin API.{' '}
                     </p>
-                    <a
-                        href='https://jamesb-cryptostats.netlify.app/'
-                        target='_blank'
-                        rel='noreferrer'
-                        style={{
-                            backgroundColor: '#5aa185',
-                            padding: '10px',
-                            borderRadius: '5px',
-                        }}
-                    >
-                        Live Demo
-                    </a>
+                    <div style={{ padding: '10px 0 30px 0' }}>
+                        <a
+                            href='https://jamesb-cryptostats.netlify.app/'
+                            target='_blank'
+                            rel='noreferrer'
+                            style={{
+                                backgroundColor: '#5aa185',
+                                padding: '10px',
+                                borderRadius: '5px',
+                            }}
+                        >
+                            Live Demo
+                        </a>
+                    </div>
                 </div>
 
                 <div>
